b08c2c894a26405c84d639f507cff2e3c99c3dec
Purice Victor <pvictor0110@gmail.com>
2023-09-10 22:51:37 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
+import Header from './components/header/Header'
+import Home from './components/header/home/Home'
 
 function App() {
-
-
   return (
     <div className='app'>
-      <p>pvictordev portfolio</p>
+      <Header/>
+
+      <main className='main'>
+        <Home/>
+      </main>
     </div>
   )
 }
---
+++ src/components/header/home/Social.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+
+         <a href="https://github.com/pvictordev" className="home__social-icon"
+        target='_blank'
+        >
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/victor-purice-204229276/" className="home__social-icon"
+        target='_blank'
+        >
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://twitter.com/wftpid" className="home__social-icon"
+        target='_blank'
+        >
+            <i class="uil uil-twitter-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
