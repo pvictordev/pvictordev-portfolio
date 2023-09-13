ce9404d80edd81029bb0e1eab6f35b0c24de44ec
Purice Victor <pvictor0110@gmail.com>
2023-09-13 21:12:49 +0300

    commit

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./services.css"
 const Services = () => {
+    const [toggleState, setToggleState] = useState(0); 
+
+    const toggleTab = (index) => {
+        setToggleState(index)
+    }
   return (
     <section className="services section" id='services'>
         <h2 className="section__title">Services</h2>
@@ -13,14 +18,17 @@ const Services = () => {
                 <div>
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">Web<br/>Developer</h3>
-                    <span className="services__button">
+                    <span className="services__button"
+                    onClick={() => toggleTab(1)}>
                         View More
-                        <i className='uil uil-arrow-right services__button-icon'></i>
+                        <i onClick={() => toggleTab(0)} className='uil uil-arrow-right services__button-icon'></i>
                     </span>
 
-                    <div className="services__modal">
+                    <div className = {
+                        toggleState === 1 ? "services__modal active-modal" : "services__modal"
+                    }>
                         <div className="services__modal-content">
-                            <i className='uil uil-times services__modal-close'></i>
+                            <i onClick = {() => toggleTab(0)} className='uil uil-times services__modal-close'></i>
 
                             <h3 className="services__modal-title">Web Developer</h3>
                             <p className='services__modal-description'>
@@ -35,28 +43,24 @@ const Services = () => {
                                         I develop the UI
                                     </p>
                                 </li>
-
                                 <li className="services__modal-service">
                                     <i className="uil uil-check-circle services__modal-icon"></i>
                                     <p className="services__modal-info">
                                         I create UX element interactions
                                     </p>
                                 </li>
-
                                 <li className="services__modal-service">
                                     <i className="uil uil-check-circle services__modal-icon"></i>
                                     <p className="services__modal-info">
                                         Web page development
                                     </p>
                                 </li>
-
                                 <li className="services__modal-service">
                                     <i className="uil uil-check-circle services__modal-icon"></i>
                                     <p className="services__modal-info">
                                        I position your company brand
                                     </p>
                                 </li>
-
                                 <li className="services__modal-service">
                                     <i className="uil uil-check-circle services__modal-icon"></i>
                                     <p className="services__modal-info">
@@ -74,14 +78,16 @@ const Services = () => {
                 <div>
                     <i className="uil uil-arrow services__icon"></i>
                     <h3 className="services__title">UI/UX <br/> Designer</h3>
-                    <span className="services__button">
+                    <span onClick={() => toggleTab(2)} className="services__button">
                         View More
-                        <i className='uil uil-arrow-right services__button-icon'></i>
+                        <i onClick={() => toggleTab(0)} className='uil uil-arrow-right services__button-icon'></i>
                     </span>
 
-                    <div className="services__modal">
+                    <div className = {
+                        toggleState === 2 ? "services__modal active-modal" : "services__modal"
+                    }>
                         <div className="services__modal-content">
-                            <i className='uil uil-times services__modal-close'></i>
+                            <i  onClick = {() => toggleTab(0)} className='uil uil-times services__modal-close'></i>
 
                             <h3 className="services__modal-title">UI/UX Designer</h3>
                             <p className='services__modal-description'>
@@ -135,14 +141,16 @@ const Services = () => {
                 <div>
                     <i className="uil uil-edit services__icon"></i>
                     <h3 className="services__title">Visual <br/> Designer</h3>
-                    <span className="services__button">
+                    <span onClick={() => toggleTab(3)} className="services__button">
                         View More
-                        <i className='uil uil-arrow-right services__button-icon'></i>
+                        <i onClick={() => toggleTab(0)} className='uil uil-arrow-right services__button-icon'></i>
                     </span>
 
-                    <div className="services__modal">
+                    <div className={
+                        toggleState === 3 ? "services__modal active-modal" : "services__modal"
+                    }>
                         <div className="services__modal-content">
-                            <i className='uil uil-times services__modal-close'></i>
+                            <i onClick={() => toggleTab(0)} className='uil uil-times services__modal-close'></i>
 
                             <h3 className="services__modal-title">Visual Designer</h3>
                             <p className='services__modal-description'>
@@ -197,3 +205,10 @@ const Services = () => {
 }
 
 export default Services
+
+var i = {}
+i.am = ['victor']
+
+i.contact = {
+    email:'bic'
+}
