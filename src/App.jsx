da9375a84b367d60b9618ef4b5d6ccb9e4747698
Purice Victor <pvictor0110@gmail.com>
2023-09-14 22:51:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Services from './components/services/Services'
 import Qualification from './components/qualification/Qualification'
 import Work from './components/work/Work'
 import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/Contact/Contact'
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import ScrollUp from './components/scrollup/ScrollUp'
 
