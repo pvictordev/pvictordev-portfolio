c8d381b45c792ec434e445644e4c0c9b9f1a4d22
Purice Victor <pvictor0110@gmail.com>
2023-09-27 20:48:00 +0300

    commit

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,13 +2,12 @@ import Work1 from "../../assets/work1.jpg"
 import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.jpg"
 import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
 
 export const projectsData = [
 
     {
         id:1,
-        image: Work1,
+        image:Work1,
         title: 'Altcoinx',
         category: 'web',
         description:'Altcoinx is a website that allows users to access real-time information about various cryptocurrencies. ',
