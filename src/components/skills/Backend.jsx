0e8ed1c39799f9c12abcc20f200bbf800c433318
Victor Purice <pvictor0110@gmail.com>
2023-10-16 10:27:34 +0300

    Update Backend.jsx

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -11,7 +11,7 @@ const Backend = () => {
                     <i class='bx bx-badge-check'></i>
                     <div>
                         <h3 className="skills__name">Node.js</h3>
-                        <span className="skills__level">Basic</span>
+                        <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
 
