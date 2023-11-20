73e92abc3b5e8f91909edf5e447414c6c06470a9
Purice Victor <pvictor0110@gmail.com>
2023-11-20 13:41:18 +0200

    Update Qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,170 +1,200 @@
-import React, {useState} from 'react';
-import "./qualification.css"; 
+import React, { useState } from "react";
+import "./qualification.css";
 
 const Qualification = () => {
-    const [toggleState, setToggleState] = useState(0); 
+  const [toggleState, setToggleState] = useState(0);
 
-    const toggleTab = (index) => {
-        setToggleState(index)
-    }
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div>
-      <section className='qualification section'>
-      <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">
-            My journey
-        </span> 
+      <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My journey</span>
 
         <div className="qualification__container container">
-            <div className="qualification__tabs">
-                <div 
-                    className={
-                        toggleState === 1 ? "qualification__button button--flex": "qualification__button button--flex"
-                    }
-                    onClick={() => toggleTab(1)}
-                >
-                    <i className="uil uil-graduation-cap qualification__icon"></i> Education
-                </div>
-
-                <div className={
-                        toggleState === 2 ? "qualification__button button--flex": "qualification__button button--flex"
-                    }
-                    onClick={() => toggleTab(2)}
-                >
-                    <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
-                </div>
+          <div className="qualification__tabs">
+            <div
+              className={
+                toggleState === 1
+                  ? "qualification__button button--flex"
+                  : "qualification__button button--flex"
+              }
+              onClick={() => toggleTab(1)}
+            >
+              <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+              Education
             </div>
 
-            <div className="qualification__sections">
-                <div 
-                className={
-                    toggleState === 1 
-                    ? "qualification__content qualification__content-active"
-                    : "qualification__content"}
-                >
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title"> Learn React </h3>
-                            <span className="qualification__subtitle">Scrimba</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2022 - 2023
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">JavaScript Essentials 1</h3>
-                            <span className="qualification__subtitle">Cisco</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2023 - 2023
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Responsive Web Design</h3>
-                            <span className="qualification__subtitle">freeCodeCamp</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2022 - 2022
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Computer Science</h3>
-                            <span className="qualification__subtitle">Lucian Blaga University</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2022 - Present
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={
-                    toggleState === 2 
-                    ? "qualification__content qualification__content-active"
-                    : "qualification__content"}
-                    >
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Web Developer</h3>
-                            <span className="qualification__subtitle">Personal Projects</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2023 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Back-End Developer</h3>
-                            <span className="qualification__subtitle"> Freelance</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2023 - Present
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Front-End Developer</h3>
-                            <span className="qualification__subtitle">Freelance</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i> 2022 - Present
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    
-                </div>
+            <div
+              className={
+                toggleState === 2
+                  ? "qualification__button button--flex"
+                  : "qualification__button button--flex"
+              }
+              onClick={() => toggleTab(2)}
+            >
+              <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+              Experience
             </div>
+          </div>
+
+          <div className="qualification__sections">
+            <div
+              className={
+                toggleState === 1
+                  ? "qualification__content qualification__content-active"
+                  : "qualification__content"
+              }
+            >
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title"> Learn React </h3>
+                  <span className="qualification__subtitle">Scrimba</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2022 - 2023
+                  </div>
+                </div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div></div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+                <div>
+                  <h3 className="qualification__title">
+                    JavaScript Essentials 1
+                  </h3>
+                  <span className="qualification__subtitle">Cisco</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2023 - 2023
+                  </div>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title">
+                    Responsive Web Design
+                  </h3>
+                  <span className="qualification__subtitle">freeCodeCamp</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2022 - 2022
+                  </div>
+                </div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div></div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+
+                <div>
+                  <h3 className="qualification__title">
+                    Postman API Fundamentals Student Expert
+                  </h3>
+                  <span className="qualification__subtitle">Postman</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2023 - 2023
+                  </div>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title">Computer Science</h3>
+                  <span className="qualification__subtitle">
+                    Lucian Blaga University of Sibiu
+                  </span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2022 - Present
+                  </div>
+                </div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className={
+                toggleState === 2
+                  ? "qualification__content qualification__content-active"
+                  : "qualification__content"
+              }
+            >
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title">Web Developer</h3>
+                  <span className="qualification__subtitle">
+                    Personal Projects
+                  </span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2023 - Present
+                  </div>
+                </div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div></div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+                <div>
+                  <h3 className="qualification__title">Back-End Developer</h3>
+                  <span className="qualification__subtitle"> Freelance</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2023 - Present
+                  </div>
+                </div>
+              </div>
+
+              <div className="qualification__data">
+                <div>
+                  <h3 className="qualification__title">Front-End Developer</h3>
+                  <span className="qualification__subtitle">Freelance</span>
+                  <div className="qualification__calendar">
+                    <i className="uil uil-calendar-alt"></i> 2022 - Present
+                  </div>
+                </div>
+
+                <div>
+                  <span className="qualification__rounder"></span>
+                  <span className="qualification__line"></span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Qualification
+export default Qualification;
