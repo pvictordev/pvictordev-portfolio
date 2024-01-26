97098b1b83489e1ca91de7d21009e9868ca11ec4
Victor Purice <victor.purice@ulbsibiu.ro>
2024-01-26 08:23:32 +0200

    fixed theme bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -38,7 +38,7 @@ const Header = ({ theme, themeToggle }) => {
           pvictordev
         </a>
 
-        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+        <div className={theme ? (Toggle ? "nav__menu-dark show-menu" : "nav__menu"): (Toggle ? "nav__menu show-menu" : "nav__menu")}>
           <ul className="nav__list grid">
             <li className="nav__item">
               <a
@@ -145,11 +145,11 @@ const Header = ({ theme, themeToggle }) => {
             </li>
           </ul>
           <i
-            className="uil uil-times nav__close"
+            className={theme ? "uil uil-times nav__close-dark": "uil uil-times nav__close" }
             onClick={() => showMenu(!Toggle)}
           ></i>
         </div>
-        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+        <div className={theme ? "nav__toggle-dark" : "nav__toggle" } onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
